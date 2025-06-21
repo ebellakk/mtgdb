4cb5ea95f96e904564d36fe520e8b3c5525c801b
Kallebe Dantas <kallmonte@gmail.com>
2025-06-21 19:23:53 -0300

    Include flexible cors configuration

--- src/main/java/com/kallebe/mtgdb/MtgdbApplication.java
+++ src/main/java/com/kallebe/mtgdb/MtgdbApplication.java
@@ -1,14 +1,30 @@
 package com.kallebe.mtgdb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MtgdbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MtgdbApplication.class, args);
-	}
+    @Value("${cors.allowed.origin.patterns}")
+    private String corsAllowedOriginPatterns;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgdbApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns(corsAllowedOriginPatterns);
+            }
+        };
+    }
 
 }
